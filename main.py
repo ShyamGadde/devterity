9d5ca935b49398d06124363d9731c4a8d4b39154
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2023-11-09 16:25:14 +0530

    feat: Display accuracy along with WPM

--- main.py
+++ main.py
@@ -12,16 +12,21 @@ def start_screen(stdscr):
 
 def display_text(stdscr, target: str, current: list, wpm=0):
     stdscr.addstr(target)
-    stdscr.addstr(2, 0, f"WPM: {wpm}")
+    incorrect_chars = 0
 
     for i, c in enumerate(current):
         correct_c = target[i]
         color = 1
         if c != correct_c:
+            incorrect_chars += 1
             color = 2
             if c == " ":
                 c = "_"
         stdscr.addstr(0, i, c, curses.color_pair(color))
+
+    accuracy = round((1 - incorrect_chars / (len(current) or 1)) * 100)
+
+    stdscr.addstr(2, 0, f"WPM: {wpm}\tAccuracy: {accuracy}%", curses.color_pair(3))
 
 
 def wpm_test(stdscr):
@@ -60,7 +65,7 @@ def wpm_test(stdscr):
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
     start_screen(stdscr)
 
