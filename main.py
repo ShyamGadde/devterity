1caf058311d2190bd3ef7dcb1137ba6a293e8de1
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-12-23 21:14:11 +0530

    Refactor code

--- main.py
+++ main.py
@@ -6,25 +6,14 @@ import time
 username = "Guest"
 
 
-def get_user_input(stdscr):
-    curses.echo()
-    user_input = stdscr.getstr().decode("utf-8")
-    curses.noecho()
-    return user_input
+def main(stdscr):
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_GREEN, -1)
+    curses.init_pair(2, curses.COLOR_RED, -1)
+    curses.init_pair(3, curses.COLOR_YELLOW, -1)
 
+    start_screen(stdscr)
 
-def start_screen(stdscr):
-    stdscr.clear()
-    stdscr.addstr("Welcome to ")
-    stdscr.addstr("Devterity!", curses.color_pair(3))
-    stdscr.addstr("\n\nPlease enter your username: ")
-    stdscr.refresh()
-
-    global username
-    username = get_user_input(stdscr)
-
-
-def display_menu(stdscr):
     while True:
         stdscr.clear()
         stdscr.addstr("\n1. Start Typing Test")
@@ -47,14 +36,62 @@ def display_menu(stdscr):
             time.sleep(1)
 
 
-def load_categories():
-    with open("words.json") as f:
-        return list(json.load(f).keys())
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to ")
+    stdscr.addstr("Devterity!", curses.color_pair(3))
+    stdscr.addstr("\n\nPlease enter your username: ")
+    stdscr.refresh()
+
+    global username
+    username = get_user_input(stdscr)
 
 
-def load_words(category: str):
-    with open("words.json") as f:
-        return json.load(f)[category]
+def get_user_input(stdscr):
+    curses.echo()
+    user_input = stdscr.getstr().decode("utf-8")
+    curses.noecho()
+    return user_input
+
+
+def wpm_test(stdscr):
+    target_text = generate_test_string(stdscr)
+    current_text = []
+    wpm = 0
+    accuracy = 100
+    start_time = time.time()
+
+    stdscr.nodelay(True)
+
+    while True:
+        wpm = calculate_wpm(start_time, current_text)
+        accuracy = calculate_accuracy(target_text, current_text)
+
+        update_window(stdscr, target_text, current_text, wpm, accuracy)
+
+        if len(current_text) == len(target_text):
+            update_leaderboard(wpm, accuracy)
+            stdscr.addstr(4, 0, "Completed!")
+            stdscr.addstr(5, 0, "Press any key to continue...")
+            stdscr.nodelay(False)
+            stdscr.getkey()
+            break
+
+        try:
+            key = stdscr.getkey()
+        except Exception:
+            continue
+
+        if key in ("KEY_BACKSPACE", "\b", "\x7f") and current_text:
+            current_text.pop()
+        elif len(current_text) < len(target_text):
+            if ord(key) == 27:
+                stdscr.addstr(4, 0, "Exiting...")
+                stdscr.refresh()
+                time.sleep(0.5)
+                break
+
+            current_text.append(key)
 
 
 def display_leaderboard(stdscr):
@@ -81,6 +118,41 @@ def display_leaderboard(stdscr):
     stdscr.getkey()
 
 
+def generate_test_string(stdscr):
+    category = select_category(stdscr)
+    words = load_words(category)
+    random.shuffle(words)
+    return " ".join(words[:20])
+
+
+def calculate_wpm(start_time, current_text):
+    time_elapsed = max(time.time() - start_time, 1)
+    return round(len(current_text) / 5 / (time_elapsed / 60))
+
+
+def calculate_accuracy(target: str, current: list):
+    incorrect_chars = sum(c != target[i] for i, c in enumerate(current))
+    return round((1 - incorrect_chars / (len(current) or 1)) * 100)
+
+
+def update_window(stdscr, target: str, current: list, wpm=0, accuracy=100):
+    stdscr.clear()
+
+    stdscr.addstr(0, 0, f"WPM: {wpm}\t\tAccuracy: {accuracy}%", curses.color_pair(3))
+    stdscr.addstr(2, 0, target)
+
+    for i, c in enumerate(current):
+        color = 1
+        if c != target[i]:
+            color = 2
+            # Replace incorrect whitespace with underscore
+            if c == " ":
+                c = "_"
+        stdscr.addstr(2, i, c, curses.color_pair(color))
+
+    stdscr.refresh()
+
+
 def update_leaderboard(wpm: int, accuracy: int):
     try:
         with open("leaderboard.json") as f:
@@ -95,27 +167,7 @@ def update_leaderboard(wpm: int, accuracy: int):
         json.dump(leaderboard, f, indent=2)
 
 
-def calculate_accuracy(target: str, current: list):
-    incorrect_chars = sum(c != target[i] for i, c in enumerate(current))
-    return round((1 - incorrect_chars / (len(current) or 1)) * 100)
-
-
-def update_window(stdscr, target: str, current: list, wpm=0, accuracy=100):
-    stdscr.addstr(0, 0, f"WPM: {wpm}\t\tAccuracy: {accuracy}%", curses.color_pair(3))
-
-    stdscr.addstr(2, 0, target)
-
-    for i, c in enumerate(current):
-        color = 1
-        if c != target[i]:
-            color = 2
-            # Replace incorrect whitespace with underscore
-            if c == " ":
-                c = "_"
-        stdscr.addstr(2, i, c, curses.color_pair(color))
-
-
-def wpm_test(stdscr):
+def select_category(stdscr):
     categories = load_categories()
     while True:
         stdscr.clear()
@@ -132,58 +184,17 @@ def wpm_test(stdscr):
         stdscr.refresh()
         time.sleep(1)
 
-    words = load_words(category)
-    random.shuffle(words)
-    target_text = " ".join(words[:20])
-
-    current_text = []
-    wpm = 0
-    accuracy = 100
-    start_time = time.time()
-    stdscr.nodelay(True)
-
-    while True:
-        time_elapsed = max(time.time() - start_time, 1)
-        wpm = round(len(current_text) / 5 / (time_elapsed / 60))
-        accuracy = calculate_accuracy(target_text, current_text)
-
-        stdscr.clear()
-        update_window(stdscr, target_text, current_text, wpm, accuracy)
-        stdscr.refresh()
-
-        if len(current_text) == len(target_text):
-            update_leaderboard(wpm, accuracy)
-            stdscr.addstr(4, 0, "Completed!")
-            stdscr.addstr(5, 0, "Press any key to continue...")
-            stdscr.nodelay(False)
-            stdscr.getkey()
-            break
-
-        try:
-            key = stdscr.getkey()
-        except Exception:
-            continue
-
-        if ord(key) == 27:
-            stdscr.addstr(4, 0, "Exiting...")
-            stdscr.refresh()
-            time.sleep(0.5)
-            break
-
-        if key in ("KEY_BACKSPACE", "\b", "\x7f") and current_text:
-            current_text.pop()
-        elif len(current_text) < len(target_text):
-            current_text.append(key)
+    return category
 
 
-def main(stdscr):
-    curses.use_default_colors()
-    curses.init_pair(1, curses.COLOR_GREEN, -1)
-    curses.init_pair(2, curses.COLOR_RED, -1)
-    curses.init_pair(3, curses.COLOR_YELLOW, -1)
+def load_categories():
+    with open("words.json") as f:
+        return list(json.load(f).keys())
 
-    start_screen(stdscr)
-    display_menu(stdscr)
+
+def load_words(category: str):
+    with open("words.json") as f:
+        return json.load(f)[category]
 
 
 if __name__ == "__main__":
